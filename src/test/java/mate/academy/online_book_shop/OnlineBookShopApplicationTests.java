38d4acb1b3a27fd9e0c6dbeef20a8e93ed370ee9
Roman Dudanets <romario220220@gmail.com>
2024-02-20 19:01:10 +0200

    firs commit

---
+++ src/test/java/mate/academy/online_book_shop/OnlineBookShopApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.academy.online_book_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
